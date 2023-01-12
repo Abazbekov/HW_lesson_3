9a2a895eea95f2b806d9e0b3534d53a3d29be0cb
Abazbekov <a.abazbekovich@gmail.com>
2023-01-12 21:35:02 +0600

    ДЗ-3 урок

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+//
          double []numbers = {1.2, 3.4, -6.4, 5.3, 12.4, -7.5, 3.7, 9.0, 8.0, 123.3, 12.9, 8.7, 12.9, 5.6, 7.8};
          double count = 0;
          int amount = 0;
